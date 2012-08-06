1aafbdc7099cfe444616b57c8af132040d22e0b1
pmzcoding <cxradio@live.com>
2012-08-06 15:44:08 -0300

    Update Plugins/project.java
    Added CHat Plugin

--- Plugins/project.java
+++ Plugins/project.java
@@ -193,9 +193,56 @@ private class host extends resolves hostname;
             }
         }
     }
-                end;
-            end;
-        end;
-    end;
-end;
+new public class Exception (bentity != int[i]=0+null Exception user/perms)
+set_new From Final private class as Exception CHAT
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import java.io.IOException; 
+
+public class Packet3Chat extends Packet {
+
+    public static int a = 119;
+    public String message;
+    private boolean c;
+
+    public Packet3Chat() {
+        this.c = true;
+    }
+
+    public Packet3Chat(String s) {
+        this(s, true);
+    }
+
+    public Packet3Chat(String s, boolean flag) {
+        this.c = true;
+
+        this.message = s;
+        this.c = flag;
+    }
+
+    public void a(DataInputStream datainputstream) throws IOException {
+        this.message = a(datainputstream, a);
+    }
+
+    public void a(DataOutputStream dataoutputstream) throws IOException {
+        a(this.message, dataoutputstream);
+    }
+
+    public void handle(NetHandler nethandler) {
+        nethandler.a(this);
+    }
+
+    public int a() {
+        return 2 + this.message.length() * 2;
+    }
+
+    public boolean isServer() {
+        return this.c;
+    }
+
+    public boolean a_() {
+        return !this.message.startsWith("/");
+    }
+}
 //PMz Modding Clan - MINECRAFT [NOT THE COD ONE]
